a18fba95f75af842fb892c99e6971527dd5e0f09
planet36 <planet36@users.noreply.github.com>
2021-01-14 10:14:54 -0500

    Increase size of stext from 256 to 512

--- dwm.c
+++ dwm.c
@@ -240,7 +240,7 @@ static void zoom(const Arg *arg);
 
 /* variables */
 static const char broken[] = "broken";
-static char stext[256];
+static char stext[512];
 static int screen;
 static int sw, sh;           /* X display screen geometry width, height */
 static int bh, blw = 0;      /* bar geometry */
