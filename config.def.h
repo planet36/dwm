9ff591dc20c3b2f331bb973bdc700ab06bf110ce
planet36 <planet36@users.noreply.github.com>
2021-01-14 12:43:44 -0500

    Add keyboard command to launch terminal
    Mod1-KeyPadEnter

--- config.def.h
+++ config.def.h
@@ -76,6 +76,7 @@ static Key keys[] = {
 	/* modifier                     key        function        argument */
 	{ MODKEY,                       XK_p,      spawn,          {.v = dmenucmd } },
 	{ MODKEY|ShiftMask,             XK_Return, spawn,          {.v = termcmd } },
+	{ MODKEY,                     XK_KP_Enter, spawn,          {.v = termcmd } },
 	{ MODKEY,                       XK_b,      togglebar,      {0} },
 	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
 	{ MODKEY,                       XK_k,      focusstack,     {.i = -1 } },
