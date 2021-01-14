585d9946bf4dafef7942fa232cb38fb536c1f163
planet36 <planet36@users.noreply.github.com>
2021-01-14 12:32:04 -0500

    Use rofi instead of dmenu

--- config.def.h
+++ config.def.h
@@ -65,7 +65,11 @@ static const Layout layouts[] = {
 
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
+#ifdef USE_ROFI
+static const char *dmenucmd[] = { "rofi", "-show", "run", NULL };
+#else
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
+#endif
 static const char *termcmd[]  = { "alacritty", NULL };
 
 static Key keys[] = {
--- config.mk
+++ config.mk
@@ -19,6 +19,6 @@ LIBS = $(XINERAMALIBS) \
        `pkg-config --libs fontconfig freetype2 x11 xft`
 
 # flags
-CPPFLAGS += -DVERSION=\"$(VERSION)\" $(XINERAMAFLAGS)
+CPPFLAGS += -DVERSION=\"$(VERSION)\" -DUSE_ROFI $(XINERAMAFLAGS)
 CFLAGS += -std=c99 -pedantic -Wall -Wno-deprecated-declarations -Os $(INCS) $(CPPFLAGS)
 LDFLAGS += $(LIBS)
