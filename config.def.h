091373278c453b130b1e2074484045301cc3dda6
planet36 <planet36@users.noreply.github.com>
2021-01-24 13:22:43 -0500

    Pass -b option to xtrlock

--- config.def.h
+++ config.def.h
@@ -71,7 +71,7 @@ static const char *dmenucmd[] = { "rofi", "-show", "run", NULL };
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
 #endif
 static const char *termcmd[]  = { "alacritty", NULL };
-static const char *lockcmd[]  = { "xtrlock", NULL };
+static const char *lockcmd[]  = { "xtrlock", "-b", NULL };
 
 static Key keys[] = {
 	/* modifier                     key        function        argument */
