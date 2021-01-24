91140f9f12ff78075ffb52d45f9142bc7ce728e1
planet36 <planet36@users.noreply.github.com>
2021-01-24 13:15:07 -0500

    Add key shortcut to lock screen

--- config.def.h
+++ config.def.h
@@ -71,12 +71,14 @@ static const char *dmenucmd[] = { "rofi", "-show", "run", NULL };
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
 #endif
 static const char *termcmd[]  = { "alacritty", NULL };
+static const char *lockcmd[]  = { "xtrlock", NULL };
 
 static Key keys[] = {
 	/* modifier                     key        function        argument */
 	{ MODKEY,                       XK_p,      spawn,          {.v = dmenucmd } },
 	{ MODKEY|ShiftMask,             XK_Return, spawn,          {.v = termcmd } },
 	{ MODKEY,                     XK_KP_Enter, spawn,          {.v = termcmd } },
+	{ MODKEY|ControlMask,           XK_l,      spawn,          {.v = lockcmd } },
 	{ MODKEY,                       XK_b,      togglebar,      {0} },
 	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
 	{ MODKEY,                       XK_k,      focusstack,     {.i = -1 } },
