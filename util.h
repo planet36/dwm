83fdafc0e7dec4a5badb62d9499d1daa07bb1e00
planet36 <planet36@users.noreply.github.com>
2021-02-28 07:47:12 -0500

    Add "noreturn" attribute to the die function

--- util.h
+++ util.h
@@ -4,5 +4,5 @@
 #define MIN(A, B)               ((A) < (B) ? (A) : (B))
 #define BETWEEN(X, A, B)        ((A) <= (X) && (X) <= (B))
 
-void die(const char *fmt, ...);
+void die(const char *fmt, ...) __attribute__ ((noreturn));
 void *ecalloc(size_t nmemb, size_t size);
