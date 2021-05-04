d42ec90772abfd64b6aa4667ebbfad728d1d5932
planet36 <planet36@users.noreply.github.com>
2021-05-04 16:21:06 -0400

    Make MIN, MAX preserve order

--- util.h
+++ util.h
@@ -1,8 +1,8 @@
 /* See LICENSE file for copyright and license details. */
 #pragma once
 
-#define MAX(A, B)               ((A) > (B) ? (A) : (B))
-#define MIN(A, B)               ((A) < (B) ? (A) : (B))
+#define MIN(A, B)               ((B) < (A) ? (B) : (A))
+#define MAX(A, B)               ((A) < (B) ? (B) : (A))
 #define BETWEEN(X, A, B)        ((A) <= (X) && (X) <= (B))
 
 void die(const char *fmt, ...) __attribute__ ((noreturn));
