012740c53a77ce9fb5815152262fee0d8e4ed84a
planet36 <planet36@users.noreply.github.com>
2022-02-13 09:17:11 -0500

    Dynamically use rofi if the program exists

--- config.mk
+++ config.mk
@@ -18,7 +18,7 @@ INCS = `pkg-config --cflags fontconfig freetype2 x11 xft`
 LIBS = `pkg-config --libs   fontconfig freetype2 x11 xft` $(XINERAMALIBS)
 
 # flags
-CPPFLAGS += -DVERSION=\"$(VERSION)\" -D_DEFAULT_SOURCE $(INCS) -DUSE_ROFI $(XINERAMAFLAGS)
+CPPFLAGS += -DVERSION=\"$(VERSION)\" -D_DEFAULT_SOURCE $(INCS) $(XINERAMAFLAGS)
 DEPFLAGS += -MMD -MP
 CFLAGS += -O2 \
 		-Wno-deprecated-declarations \
@@ -26,3 +26,9 @@ CFLAGS += -O2 \
 		-Wno-sign-compare \
 		-Wno-unused-parameter
 LDLIBS += $(LIBS)
+
+ROFI := $(shell command -v rofi)
+ifdef ROFI
+	CPPFLAGS += -DUSE_ROFI
+endif
+undefine ROFI
