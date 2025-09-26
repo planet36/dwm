8b58f8599d5802d12480421e0e7a7b6707f9e0f9
Steven Ward <planet36@users.noreply.github.com>
2025-09-26 12:45:19 -0400

    Move MIN after MAX

--- util.h
+++ util.h
@@ -1,8 +1,8 @@
 /* See LICENSE file for copyright and license details. */
 #pragma once
 
-#define MIN(A, B)               ((B) < (A) ? (B) : (A))
 #define MAX(A, B)               ((A) < (B) ? (B) : (A))
+#define MIN(A, B)               ((B) < (A) ? (B) : (A))
 #define BETWEEN(X, A, B)        ((A) <= (X) && (X) <= (B))
 #define LENGTH(X)               (sizeof (X) / sizeof (X)[0])
 
