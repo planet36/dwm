57e7b301c51ae209c30ce454b9d57e217025181d
Steven Ward <planet36@users.noreply.github.com>
2025-12-09 12:33:32 -0500

    Change terminal from xterm to kitty

--- config.h
+++ config.h
@@ -69,7 +69,7 @@ static const Layout layouts[] = {
 /* helper for spawning shell commands in the pre dwm-5.0 fashion */
 #define SHCMD(cmd) { .v = (const char*[]){ "/bin/sh", "-c", cmd, NULL } }
 
-#define TERM "xterm"
+#define TERM "kitty"
 
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
