05194f866dc13ab02d0e61ed48529d64cf53f5aa
Steven Ward <planet36@users.noreply.github.com>
2026-02-17 09:37:47 -0500

    Add ability to spawn calendar notification

--- config.h
+++ config.h
@@ -71,6 +71,8 @@ static const Layout layouts[] = {
 
 #define TERM "kitty"
 
+#define CALCMD "dunstify -a calendar -- \"$(date '+%B %Y')\" \"$(cal | tail -n +2)\""
+
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4,
@@ -87,6 +89,7 @@ static const Key keys[] = {
 	{ MODKEY,                       XK_KP_Enter,     spawn,            {.v = termcmd} },
 	{ MODKEY|ControlMask,           XK_l,            spawn,            {.v = lockcmd} },
 	{ MODKEY,                       XK_e,            spawn,            {.v = scratchcmd} },
+	{ MODKEY,                       XK_c,            spawn,            SHCMD(CALCMD) },
 	{ MODKEY,                       XK_b,            togglebar,        {0} },
 	{ MODKEY,                       XK_j,            focusstack,       {.i = +1} },
 	{ MODKEY,                       XK_k,            focusstack,       {.i = -1} },
@@ -133,6 +136,7 @@ static const Button buttons[] = {
 	{ ClkLtSymbol,          0,              Button1,        setlayout,      {0} },
 	{ ClkLtSymbol,          0,              Button3,        setlayout,      {.v = &layouts[2]} },
 	{ ClkWinTitle,          0,              Button2,        zoom,           {0} },
+	{ ClkStatusText,        0,              Button1,        spawn,          SHCMD(CALCMD) },
 	{ ClkStatusText,        0,              Button2,        spawn,          {.v = termcmd } },
 	{ ClkClientWin,         MODKEY,         Button1,        movemouse,      {0} },
 	{ ClkClientWin,         MODKEY,         Button2,        togglefloating, {0} },
